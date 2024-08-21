1cf46d4fc0d673308048b630aa33a6a29379acca
vikaz2003 <vikaskunch03@gmail.com>
2024-08-21 12:03:28 +0530

    Prepared_base_model

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.Kidney_Disease_Classification import logger
 from src.Kidney_Disease_Classification.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from src.Kidney_Disease_Classification.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 
 STAGE_NAME="Data Ingestion Stage"
@@ -14,3 +15,18 @@ if __name__ == '__main__':
     except Exception as e:
         logger.exception(e)
         raise e
+    
+
+STAGE_NAME="Preapare_base_model"
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = PrepareBaseModelTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+        
+    except Exception as e:
+        logger.exception(e)
+        raise e
+    
---
+++ src/Kidney_Disease_Classification/pipeline/stage_02_prepare_base_model.py
@@ -0,0 +1,30 @@
+from Kidney_Disease_Classification.config.configuration import ConfigurationManager
+from Kidney_Disease_Classification.components.prepare_base_model import PrepareBaseModel
+from Kidney_Disease_Classification import logger
+
+
+STAGE_NAME="Prepare_base_model"
+
+class PrepareBaseModelTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        prepare_base_model_config = config.get_prepare_base_model_config()
+        prepare_base_model = PrepareBaseModel(config=prepare_base_model_config)
+        prepare_base_model.get_base_model()
+        prepare_base_model.update_base_model()
+
+
+    
+if __name__ == '__main__':
+    try:
+        logger.info(f"*******************")
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = PrepareBaseModelTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
