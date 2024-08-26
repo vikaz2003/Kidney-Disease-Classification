13f9336d0f351811aef3abe3dabf390aee976648
vikaz2003 <vikaskunch03@gmail.com>
2024-08-26 11:48:16 +0530

    modeladded

--- src/Kidney_Disease_Classification/pipeline/prediction.py
+++ src/Kidney_Disease_Classification/pipeline/prediction.py
@@ -8,7 +8,7 @@ class PredictionPipeline:
     def __init__(self,filename):
         self.filename=filename
     def predict(self):
-        model=load_model(os.path.join("artifacts","training","model.h5"))
+        model=load_model(os.path.join("model","model.h5"))
 
         imagename=self.filename
         test_image=image.load_img(imagename,target_size=(224,224))
